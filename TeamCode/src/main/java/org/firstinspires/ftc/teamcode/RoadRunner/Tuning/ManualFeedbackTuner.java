f91266cbb54ea463cbca8b430867b189b1dad271
Sunking2020 <144185841+Sunking2020@users.noreply.github.com>
2024-11-02 09:33:22 -0500

    Added RoadRunner and IMu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/Tuning/ManualFeedbackTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/Tuning/ManualFeedbackTuner.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.teamcode.RoadRunner.ThreeDeadWheelLocalizer;
 import org.firstinspires.ftc.teamcode.RoadRunner.TwoDeadWheelLocalizer;
 
 public final class ManualFeedbackTuner extends LinearOpMode {
-    public static double DISTANCE = 64;
+    public static double DISTANCE = 30;
 
     @Override
     public void runOpMode() throws InterruptedException {
